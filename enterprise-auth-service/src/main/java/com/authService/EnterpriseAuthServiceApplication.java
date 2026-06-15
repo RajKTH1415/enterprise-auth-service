9d936aec17c984bd5dece328f5d4237bcd19ba57
Rajkumar prasad <rajkumarprasadkth@gmail.com>
2026-06-15 11:24:00 +0530

    first commit

---
+++ enterprise-auth-service/src/main/java/com/authService/EnterpriseAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.authService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAuthServiceApplication.class, args);
+	}
+
+}
